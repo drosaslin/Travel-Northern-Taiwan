cc3860a54609b940c6d36ac4a26a3d529edae849
drosaslin <davidrosas.xc@gmail.com>
2018-07-13 22:06:33 +0800

    transitions from main actity to login, signup, and language activities added

--- travel_northern_taiwan/app/src/main/java/com/example/android/travelnortherntaiwan/MainActivity.java
+++ travel_northern_taiwan/app/src/main/java/com/example/android/travelnortherntaiwan/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.travelnortherntaiwan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openLoginActivity(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignupActivity(View v) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLanguageActivity(View v) {
+        Intent intent = new Intent(this, LanguageActivity.class);
+        startActivity(intent);
     }
 }
